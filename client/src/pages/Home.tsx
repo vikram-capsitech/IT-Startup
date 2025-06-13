17c8a004ddaa65fb8bf33fd07860817a564bea1f
vikram-capsitech <vikramsingh.capsitech@gmail.com>
2025-06-13 18:52:36 +0530

    Path change

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/layout/navbar";
-import Footer from "@/components/layout/footer";
+import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/hero";
 import Services from "@/components/sections/services";
 import Projects from "@/components/sections/projects";
