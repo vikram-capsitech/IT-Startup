cb5753c72564e0eb12dccdd65a495e1fc09f63bc
vikram-capsitech <vikramsingh.capsitech@gmail.com>
2025-06-13 18:54:14 +0530

    Minor Change 1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
+import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
 function Router() {
